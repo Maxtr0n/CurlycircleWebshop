604f5b75edc9ec485ce3410ca8b67ef5dcfb8ae8
Schütz Máté <schutz.mate@gmail.com>
2021-11-18 11:33:57 +0100

    feat(api): add cors

--- CurlycircleWebApi/CurlycircleWebApi/Startup.cs
+++ CurlycircleWebApi/CurlycircleWebApi/Startup.cs
@@ -37,11 +37,18 @@ namespace CurlycircleWebApi
 
             services.AddDbContext<ApplicationDbContext>(options =>
               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+          app.UseCors(options =>
+            options.WithOrigins("http://localhost:4200")
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
