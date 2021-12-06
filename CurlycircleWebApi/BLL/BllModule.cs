a12f11a0f7342b54d4a02736beb7077cfaecdb22
Schütz Máté <schutz.mate@gmail.com>
2021-12-07 00:07:30 +0100

    added modules with autofac

--- CurlycircleWebApi/BLL/BllModule.cs
+++ CurlycircleWebApi/BLL/BllModule.cs
@@ -6,20 +6,17 @@ using System.Text;
 using System.Threading.Tasks;
 using Autofac;
 
-namespace DAL
+namespace BLL
 {
-  public class DalModule : Autofac.Module
+  public class BllModule : Autofac.Module
   {
     protected override void Load(ContainerBuilder builder)
     {
       base.Load(builder);
 
-      var assembly = Assembly.GetAssembly(typeof(DalModule));
+      var assembly = Assembly.GetAssembly(typeof(BllModule));
       builder.RegisterAssemblyTypes(assembly)
-        .Where(type => type.Name.EndsWith("Repository"))
-        .AsImplementedInterfaces()
-        .InstancePerLifetimeScope();
-      builder.RegisterType<UnitOfWork>()
+        .Where(type => type.Name.EndsWith("Service"))
         .AsImplementedInterfaces()
         .InstancePerLifetimeScope();
     }
