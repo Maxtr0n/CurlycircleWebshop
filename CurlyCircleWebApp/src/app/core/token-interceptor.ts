33ad740092f37d0810ebf7b5c9544efcc3153d9e
Schütz Máté <schutz.mate@gmail.com>
2022-08-28 14:14:32 +0200

    feat(webapp): refresh token refactor start

--- CurlyCircleWebApp/src/app/core/token-interceptor.ts
+++ CurlyCircleWebApp/src/app/core/token-interceptor.ts
@@ -29,14 +29,56 @@ export class TokenInterceptor implements HttpInterceptor {
     constructor(private readonly authService: AuthService) { }
 
     intercept(
-        request: HttpRequest<any>,
+        req: HttpRequest<any>,
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
-        request = request.clone({
+        let authReq = req;
+        const token = this.authService.getAccessToken();
+        if (token != null) {
+            authReq = this.addTokenHeader(req, token);
+        }
+
+        return next.handle(authReq).pipe(catchError(error => {
+            if (error instanceof HttpErrorResponse && !authReq.url.includes('auth/login') && error.status === 401) {
+                return this.handle401Error(authReq, next);
+            }
+            return throwError(() => new Error(error));
+        }));
+    }
+
+    private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+        if (!this.isRefreshing) {
+            this.isRefreshing = true;
+            this.refreshTokenSubject.next(null);
+            const token = this.authService.getRefreshToken();
+            if (token)
+                return this.authService.refreshToken().pipe(
+                    switchMap((token: TokenViewModel) => {
+                        this.isRefreshing = false;
+                        this.refreshTokenSubject.next(token.refreshToken);
+
+                        return next.handle(this.addTokenHeader(request, token.accessToken));
+                    }),
+                    catchError((err) => {
+                        this.isRefreshing = false;
+
+                        this.authService.logout();
+                        return throwError(() => new Error(err));
+                    })
+                );
+        }
+        return this.refreshTokenSubject.pipe(
+            filter(token => token !== null),
+            take(1),
+            switchMap((token) => next.handle(this.addTokenHeader(request, token)))
+        );
+    }
+
+    private addTokenHeader(request: HttpRequest<any>, token: string) {
+        return request.clone({
             setHeaders: {
-                Authorization: `Bearer ${this.authService.getAccessToken()}`
+                Authorization: `Bearer ${token}`
             }
         });
-        return next.handle(request);
     }
 }
