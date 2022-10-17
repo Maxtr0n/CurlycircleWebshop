38bef3ef2c25b0b89bf0c5a0515d71c34e02d048
Schütz Máté <schutz.mate@gmail.com>
2022-10-17 12:27:03 +0200

    feat(webapi): unit test work

---
+++ CurlycircleWebApi/UnitTests/Services/MaterialServiceTests.cs
@@ -0,0 +1,133 @@
+﻿using AutoMapper;
+using BLL.Dtos;
+using BLL.Services;
+using BLL.ViewModels;
+using Domain.Entities;
+using Domain.Exceptions;
+using Domain.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Services
+{
+    public class MaterialServiceTests
+    {
+        private readonly Mock<IMaterialRepository> _materialRepositoryStub;
+        private readonly Mock<IUnitOfWork> _unitOfWorkStub;
+        private readonly Mock<IMapper> _mapperStub;
+        private readonly MaterialService _materialService;
+
+        public MaterialServiceTests()
+        {
+            _materialRepositoryStub = new Mock<IMaterialRepository>();
+            _unitOfWorkStub = new Mock<IUnitOfWork>();
+            _mapperStub = new Mock<IMapper>();
+            _materialService = new MaterialService(_materialRepositoryStub.Object, _unitOfWorkStub.Object, _mapperStub.Object);
+        }
+
+        [Fact]
+        public async Task GetAllMaterialsAsync_ReturnsAllMaterials()
+        {
+            // Arrange
+            var materials = new List<Material>()
+            {
+                new Material() { Id = 1, Name = "Szövet" },
+                new Material() { Id = 2, Name = "Fém" }
+            };
+
+            var materialsViewModel = new MaterialsViewModel();
+
+            materialsViewModel.Materials = new List<MaterialViewModel>()
+            {
+                new MaterialViewModel() {  Id = 1, Name= "Szövet"},
+                new MaterialViewModel() {  Id = 2, Name = "Fém" },
+            };
+
+
+            _materialRepositoryStub.Setup(repo => repo.GetAllAsync().Result)
+                .Returns(materials);
+
+            _mapperStub.Setup(mapper => mapper.Map<MaterialsViewModel>(materials))
+                .Returns(materialsViewModel);
+
+            //Act
+            var result = await _materialService.GetAllMaterialsAsync();
+            var resultList = result.Materials.ToList();
+
+            //Assert
+            Assert.IsType<MaterialsViewModel>(result);
+            Assert.Equal(2, resultList.Count);
+        }
+
+        [Fact]
+        public async Task FindMaterialByIdAsync_WithValidId_ReturnsExactMaterial()
+        {
+            // Arrange
+            var material = new Material() { Id = 1, Name = "Szövet" };
+
+            _materialRepositoryStub.Setup(repo => repo.GetMaterialByIdAsync(1).Result)
+                .Returns(material);
+
+            var materialViewModel = new MaterialViewModel() { Id = 1, Name = "Szövet" };
+
+            _mapperStub.Setup(mapper => mapper.Map<MaterialViewModel>(material))
+                .Returns(materialViewModel);
+
+            //Act
+            var result = await _materialService.FindMaterialByIdAsync(1);
+
+            //Assert
+            Assert.IsType<MaterialViewModel>(result);
+            Assert.Equal(1, result.Id);
+            Assert.Equal("Szövet", result.Name);
+        }
+
+        [Fact]
+        public async Task FindMaterialByIdAsync_WithInvalidId_ThrowsEntityNotFoundException()
+        {
+            //Arrange
+            _materialRepositoryStub.Setup(repo => repo.GetMaterialByIdAsync(1).Result)
+                .Throws(new EntityNotFoundException($"Material with id {1} not found."));
+
+            //Act & Assert
+            await Assert.ThrowsAsync<EntityNotFoundException>(() => _materialService.FindMaterialByIdAsync(1));
+        }
+
+        [Fact]
+        public async Task CreateMaterialAsync_WithValidName_CreatesExactMaterial()
+        {
+            // Arrange
+            var materialDto = new MaterialUpsertDto() { Name = "Szövet" };
+            var material = new Material() { Id = 1, Name = "Szövet" };
+
+            _materialRepositoryStub.Setup(repo => repo.AddMaterial(material))
+                .Returns(1);
+
+            _mapperStub.Setup(mapper => mapper.Map<Material>(materialDto))
+                .Returns(material);
+
+            //Act
+            var result = await _materialService.CreateMaterialAsync(materialDto);
+
+            //Assert
+            Assert.IsType<EntityCreatedViewModel>(result);
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public async Task DeleteMaterialAsync_WithValidId_CallsDeleteMethodInRepository()
+        {
+            // Arrange
+
+            //Act
+            await _materialService.DeleteMaterialAsync(1);
+
+            //Assert
+            _materialRepositoryStub.Verify(c => c.DeleteMaterialAsync(1), Times.Once);
+        }
+    }
+}
---
+++ CurlycircleWebApi/UnitTests/Services/PatternServiceTests.cs
@@ -0,0 +1,133 @@
+﻿using AutoMapper;
+using BLL.Dtos;
+using BLL.Services;
+using BLL.ViewModels;
+using Domain.Entities;
+using Domain.Exceptions;
+using Domain.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Services
+{
+    public class PatternServiceTests
+    {
+        private readonly Mock<IPatternRepository> _patternRepositoryStub;
+        private readonly Mock<IUnitOfWork> _unitOfWorkStub;
+        private readonly Mock<IMapper> _mapperStub;
+        private readonly PatternService _patternService;
+
+        public PatternServiceTests()
+        {
+            _patternRepositoryStub = new Mock<IPatternRepository>();
+            _unitOfWorkStub = new Mock<IUnitOfWork>();
+            _mapperStub = new Mock<IMapper>();
+            _patternService = new PatternService(_patternRepositoryStub.Object, _unitOfWorkStub.Object, _mapperStub.Object);
+        }
+
+        [Fact]
+        public async Task GetAllPatternsAsync_ReturnsAllPatterns()
+        {
+            // Arrange
+            var patterns = new List<Pattern>()
+            {
+                new Pattern() { Id = 1, Name = "Csíkos" },
+                new Pattern() { Id = 2, Name = "Pöttyös" }
+            };
+
+            var patternsViewModel = new PatternsViewModel();
+
+            patternsViewModel.Patterns = new List<PatternViewModel>()
+            {
+                new PatternViewModel() {  Id = 1, Name= "Csíkos"},
+                new PatternViewModel() {  Id = 2, Name = "Pöttyös" },
+            };
+
+
+            _patternRepositoryStub.Setup(repo => repo.GetAllAsync().Result)
+                .Returns(patterns);
+
+            _mapperStub.Setup(mapper => mapper.Map<PatternsViewModel>(patterns))
+                .Returns(patternsViewModel);
+
+            //Act
+            var result = await _patternService.GetAllPatternsAsync();
+            var resultList = result.Patterns.ToList();
+
+            //Assert
+            Assert.IsType<PatternsViewModel>(result);
+            Assert.Equal(2, resultList.Count);
+        }
+
+        [Fact]
+        public async Task FindPatternByIdAsync_WithValidId_ReturnsExactPattern()
+        {
+            // Arrange
+            var pattern = new Pattern() { Id = 1, Name = "Csíkos" };
+
+            _patternRepositoryStub.Setup(repo => repo.GetPatternByIdAsync(1).Result)
+                .Returns(pattern);
+
+            var patternViewModel = new PatternViewModel() { Id = 1, Name = "Csíkos" };
+
+            _mapperStub.Setup(mapper => mapper.Map<PatternViewModel>(pattern))
+                .Returns(patternViewModel);
+
+            //Act
+            var result = await _patternService.FindPatternByIdAsync(1);
+
+            //Assert
+            Assert.IsType<PatternViewModel>(result);
+            Assert.Equal(1, result.Id);
+            Assert.Equal("Csíkos", result.Name);
+        }
+
+        [Fact]
+        public async Task FindPatternByIdAsync_WithInvalidId_ThrowsEntityNotFoundException()
+        {
+            //Arrange
+            _patternRepositoryStub.Setup(repo => repo.GetPatternByIdAsync(1).Result)
+                .Throws(new EntityNotFoundException($"Pattern with id {1} not found."));
+
+            //Act & Assert
+            await Assert.ThrowsAsync<EntityNotFoundException>(() => _patternService.FindPatternByIdAsync(1));
+        }
+
+        [Fact]
+        public async Task CreatePatternAsync_WithValidName_CreatesExactPattern()
+        {
+            // Arrange
+            var patternDto = new PatternUpsertDto() { Name = "Csíkos" };
+            var pattern = new Pattern() { Id = 1, Name = "Csíkos" };
+
+            _patternRepositoryStub.Setup(repo => repo.AddPattern(pattern))
+                .Returns(1);
+
+            _mapperStub.Setup(mapper => mapper.Map<Pattern>(patternDto))
+                .Returns(pattern);
+
+            //Act
+            var result = await _patternService.CreatePatternAsync(patternDto);
+
+            //Assert
+            Assert.IsType<EntityCreatedViewModel>(result);
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public async Task DeletePatternAsync_WithValidId_CallsDeleteMethodInRepository()
+        {
+            // Arrange
+
+            //Act
+            await _patternService.DeletePatternAsync(1);
+
+            //Assert
+            _patternRepositoryStub.Verify(c => c.DeletePatternAsync(1), Times.Once);
+        }
+    }
+}
