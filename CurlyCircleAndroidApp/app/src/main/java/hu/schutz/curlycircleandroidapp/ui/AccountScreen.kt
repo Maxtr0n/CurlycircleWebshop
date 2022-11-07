342d0204755897d8174297edd250c2b9b41b8408
Schütz Máté <schutz.mate@gmail.com>
2022-11-07 10:46:34 +0100

    feat(android): added dependencies

--- CurlyCircleAndroidApp/app/src/main/java/hu/schutz/curlycircleandroidapp/ui/AccountScreen.kt
+++ CurlyCircleAndroidApp/app/src/main/java/hu/schutz/curlycircleandroidapp/ui/AccountScreen.kt
@@ -1,9 +1,25 @@
 package hu.schutz.curlycircleandroidapp.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import hu.schutz.curlycircleandroidapp.ui.theme.CurlyCircleAndroidAppTheme
 
 @Composable
 fun AccountScreen() {
-    Text(text = "Account screen")
+    Surface {
+        Column {
+            Text(text = "Személyes adatok")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AccountScreenPreview() {
+    CurlyCircleAndroidAppTheme {
+        AccountScreen()
+    }
 }
--- CurlyCircleAndroidApp/app/src/main/java/hu/schutz/curlycircleandroidapp/ui/ShopScreen.kt
+++ CurlyCircleAndroidApp/app/src/main/java/hu/schutz/curlycircleandroidapp/ui/ShopScreen.kt
@@ -1,9 +1,26 @@
 package hu.schutz.curlycircleandroidapp.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import hu.schutz.curlycircleandroidapp.ui.theme.CurlyCircleAndroidAppTheme
 
 @Composable
 fun ShopScreen() {
-    Text(text = "Shop screen")
+    Column {
+        Text(text = "Shop screen")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Yes")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ShopScreenPreview() {
+    CurlyCircleAndroidAppTheme {
+        ShopScreen()
+    }
 }
