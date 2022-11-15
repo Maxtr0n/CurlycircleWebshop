1379de445560a926e847f89d4d0c1a267c27f7e6
Schütz Máté <schutz.mate@gmail.com>
2022-11-15 12:25:41 +0100

    feat(android): di work

---
+++ CurlyCircleAndroidApp/app/src/main/java/hu/schutz/curlycircleandroidapp/di/DataSourceModule.kt
@@ -0,0 +1,127 @@
+package hu.schutz.curlycircleandroidapp.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import hu.schutz.curlycircleandroidapp.data.source.*
+import hu.schutz.curlycircleandroidapp.data.source.local.*
+import hu.schutz.curlycircleandroidapp.data.source.remote.*
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class RemoteDataSource
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class LocalDataSource
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataSourceModule {
+
+    @Singleton
+    @RemoteDataSource
+    @Provides
+    fun provideRemoteProductCategoriesDataSource(
+        api: CurlyCircleApi,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): ProductCategoriesDataSource {
+        return ProductCategoriesRemoteDataSource(api, ioDispatcher)
+    }
+
+    @Singleton
+    @LocalDataSource
+    @Provides
+    fun provideLocalProductCategoriesDataSource(
+        database: CurlyCircleDatabase,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+        ): ProductCategoriesDataSource {
+        return ProductCategoriesLocalDataSource(database.productCategoriesDao(), ioDispatcher)
+    }
+
+    @Singleton
+    @RemoteDataSource
+    @Provides
+    fun provideRemoteProductsDataSource(
+        api: CurlyCircleApi,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): ProductsDataSource {
+        return ProductsRemoteDataSource(api, ioDispatcher)
+    }
+
+    @Singleton
+    @LocalDataSource
+    @Provides
+    fun provideLocalProductsDataSource(
+        database: CurlyCircleDatabase,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): ProductsDataSource {
+        return ProductsLocalDataSource(database.productsDao(), ioDispatcher)
+    }
+
+    @Singleton
+    @RemoteDataSource
+    @Provides
+    fun provideRemoteColorsDataSource(
+        api: CurlyCircleApi,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): ColorsDataSource {
+        return ColorsRemoteDataSource(api, ioDispatcher)
+    }
+
+    @Singleton
+    @LocalDataSource
+    @Provides
+    fun provideLocalColorsDataSource(
+        database: CurlyCircleDatabase,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): ColorsDataSource {
+        return ColorsLocalDataSource(database.colorsDao(), ioDispatcher)
+    }
+
+    @Singleton
+    @RemoteDataSource
+    @Provides
+    fun provideRemoteMaterialsDataSource(
+        api: CurlyCircleApi,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): MaterialsDataSource {
+        return MaterialsRemoteDataSource(api, ioDispatcher)
+    }
+
+    @Singleton
+    @LocalDataSource
+    @Provides
+    fun provideLocalMaterialsDataSource(
+        database: CurlyCircleDatabase,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): MaterialsDataSource {
+        return MaterialsLocalDataSource(database.materialsDao(), ioDispatcher)
+    }
+
+    @Singleton
+    @RemoteDataSource
+    @Provides
+    fun provideRemotePatternsDataSource(
+        api: CurlyCircleApi,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): PatternsDataSource {
+        return PatternsRemoteDataSource(api, ioDispatcher)
+    }
+
+    @Singleton
+    @LocalDataSource
+    @Provides
+    fun provideLocalPatternsDataSource(
+        database: CurlyCircleDatabase,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): PatternsDataSource {
+        return PatternsLocalDataSource(database.patternsDao(), ioDispatcher)
+    }
+}
+
---
+++ CurlyCircleAndroidApp/app/src/main/java/hu/schutz/curlycircleandroidapp/di/RepositoryModule.kt
@@ -0,0 +1,65 @@
+package hu.schutz.curlycircleandroidapp.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import hu.schutz.curlycircleandroidapp.data.repository.*
+import hu.schutz.curlycircleandroidapp.data.source.*
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideProductCategoriesRepository(
+        @RemoteDataSource remoteDataSource: ProductCategoriesDataSource,
+        @LocalDataSource localDataSource: ProductCategoriesDataSource,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): ProductCategoriesRepository {
+        return DefaultProductCategoriesRepository(remoteDataSource, localDataSource, ioDispatcher)
+    }
+
+    @Singleton
+    @Provides
+    fun provideProductsRepository(
+        @RemoteDataSource remoteDataSource: ProductsDataSource,
+        @LocalDataSource localDataSource: ProductsDataSource,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): ProductsRepository {
+        return DefaultProductsRepository(remoteDataSource, localDataSource, ioDispatcher)
+    }
+
+    @Singleton
+    @Provides
+    fun provideColorsRepository(
+        @RemoteDataSource remoteDataSource: ColorsDataSource,
+        @LocalDataSource localDataSource: ColorsDataSource,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): ColorsRepository {
+        return DefaultColorsRepository(remoteDataSource, localDataSource, ioDispatcher)
+    }
+
+    @Singleton
+    @Provides
+    fun provideMaterialsRepository(
+        @RemoteDataSource remoteDataSource: MaterialsDataSource,
+        @LocalDataSource localDataSource: MaterialsDataSource,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): MaterialsRepository {
+        return DefaultMaterialsRepository(remoteDataSource, localDataSource, ioDispatcher)
+    }
+
+    @Singleton
+    @Provides
+    fun providePatternsRepository(
+        @RemoteDataSource remoteDataSource: PatternsDataSource,
+        @LocalDataSource localDataSource: PatternsDataSource,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): PatternsRepository {
+        return DefaultPatternsRepository(remoteDataSource, localDataSource, ioDispatcher)
+    }
+}
